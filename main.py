09ec9be57580719c4f5d1fbce8ed20953c939f9d
tksan66 <eto.hero@gmail.com>
2026-01-26 22:52:25 +0900

    リファクタリング実施

--- main.py
+++ main.py
@@ -5,58 +5,9 @@ import os
 import streamlit as st
 import psycopg
 import pandas as pd
+import connect_neo4j
+import connect_postgredb
 
-load_dotenv()
-NEO4J_URI=os.getenv("NEO4J_URI")
-NEO4J_USER=os.getenv("NEO4J_USER")
-NEO4J_PASSWORD=os.getenv("NEO4J_PASSWORD")
-PORT=os.getenv("POSTGRES_PORT")
-DBNAME=os.getenv("POSTGRES_DBNAME")
-USER=os.getenv("POSTGRES_USER")
-PASSWORD=os.getenv("POSTGRES_PASSWORD")
-
-
-driver = GraphDatabase.driver(
-    NEO4J_URI,
-    auth=(NEO4J_USER, NEO4J_PASSWORD)
-)
-
-def get_pump_context(pump_id):
-    with driver.session() as session:
-        result = session.run("""
-            MATCH (p:Pump {id: $pid})
-            OPTIONAL MATCH (p)-[:HAS_SENSOR]->(s)
-            OPTIONAL MATCH (p)-[:HAS_MAINTENANCE]->(m)
-            RETURN
-              p.name AS pump,
-              collect(s.type) AS sensors_type,
-              collect(s.id) AS sensors_id,
-              collect(s.external_timeseries_key) AS external_key,
-              collect(m.date) AS maint_dates
-        """, pid=pump_id)
-
-        record = result.single()
-        return record['pump'], record['sensors_type'],record['sensors_id'],record['external_key'],record['maint_dates']
-    
-conninfo = f"host=127.0.0.1 port={PORT} dbname={DBNAME} user={USER} password={PASSWORD}"
-
-def fetch_timeseries(sensor_key: str, start_ts: str, end_ts: str, conninfo: str) -> pd.DataFrame:
-    with psycopg.connect(conninfo) as conn:
-        with conn.cursor() as cur:
-            cur.execute(
-                """
-                SELECT ts, value
-                FROM sensor_readings
-                WHERE sensor_id = %s
-                  AND ts >= %s::timestamptz
-                  AND ts <= %s::timestamptz
-                ORDER BY ts
-                """,
-                (sensor_key, start_ts, end_ts),
-            )
-            rows = cur.fetchall()
-
-    return pd.DataFrame(rows, columns=["ts", "value"])
 
 def detect_anomaly_zscore(df: pd.DataFrame, window: int = 6, z_th: float = 1.5) -> pd.DataFrame:
     df = df.copy()
@@ -66,8 +17,6 @@ def detect_anomaly_zscore(df: pd.DataFrame, window: int = 6, z_th: float = 1.5) 
     df["is_anomaly"] = df["z"].abs() >= z_th
     return df[df["is_anomaly"] == True]
 
-
-
     
 llm = ChatOpenAI(model="gpt-4o-mini", temperature=0)
 
@@ -76,7 +25,7 @@ st.title("Industrial AI Demo")
 
 pump_id = st.selectbox(
     "Select Pump",
-    ["Pump-23"]
+    ["Pump-23","Pump-25"]
 )
 
 question = st.text_input(
@@ -84,10 +33,15 @@ question = st.text_input(
         "オペレーション上のリスクはなにかありますか"
     )
 
-pump_info,sensors_type,sensors_id,sensors_key, main_date= get_pump_context(pump_id)
+try:
+    pump_info,sensors_type,sensors_id,sensors_key, main_date= connect_neo4j.get_pump_context(pump_id)
+except:
+    pump_info,sensors_type,sensors_id,sensors_key, main_date="該当のpumpは存在しません","","","",""
+
+    
 context = ""
 for num in range(len(sensors_key)):
-    df = fetch_timeseries(sensors_key[num], "2025-01-17 00:00:00+00","2025-01-17 23:00:00+00",conninfo)
+    df = connect_postgredb.fetch_timeseries(sensors_key[num], "2025-01-17 00:00:00+00","2025-01-17 23:00:00+00")
     df_result = detect_anomaly_zscore(df)
 
     context += f"""
